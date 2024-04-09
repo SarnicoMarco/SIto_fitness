89e6ec19caad15086af78db56f76a2e1c26e2de3
SarnicoMarco <91139257+SarnicoMarco@users.noreply.github.com>
2024-04-09 18:13:06 +0200

    albygchesalva

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import sqlite3 as sq
-
+from flask import Flask
+from flask import render_template   
+app = Flask(__name__)
 conn = sq.connect('db.sqlite3')
 
 with open('db.sql') as f:
     conn.executescript(f.read())
 conn.commit()
 conn.close()
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
